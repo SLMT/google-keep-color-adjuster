b5ab9e76ff2a86e0731b2a5b91624c0a41637ac0
slmt <sam123456777@gmail.com>
2015-09-05 17:06:26 +0800

    Change the replacement method

--- data/color-adjuster.js
+++ data/color-adjuster.js
@@ -1,31 +1,44 @@
 
-// 顏色依序是：Default, Red, Orange, Yellow, Gray, Bule, Teal, Green
-var oldColors = ["250, 250, 250", "255, 109, 63", "255, 155, 0", "255, 218, 0", "184, 196, 201", "63, 195, 255", "28, 232, 181", "149, 214, 65"];
-var newColors = ["250, 250, 250", "241, 169, 160", "244, 179, 80", "255, 231, 91", "184, 196, 201", "92, 151, 191", "162, 222, 208", "189, 228, 101"];
+// 定義要替換的顏色
+var replaceMap = {
+	"250, 250, 250" : "250, 250, 250", // Default
+	"255, 109, 63" : "241, 169, 160", // Red
+	"255, 155, 0" : "244, 179, 80", // Orange
+	"255, 218, 0" : "255, 231, 91", // Yellow
+	"184, 196, 201" : "184, 196, 201", // Gray
+	"63, 195, 255" : "92, 151, 191", // Bule
+	"28, 232, 181" : "162, 222, 208", // Teal
+	"149, 214, 65" : "189, 228, 101" // Green
+};
+
+// Regular Expression
+// 規則：數字 + [空白或逗號] + 數字 + [空白或逗號] + 數字
+var rgbColorPattern = /\d+[\s,]+\d+[\s,]+\d+/;
 
 // 一些指標性名稱
-var targetContainerName = "neVct-SX9D7d-ObfsIf";
+var backgroundColorClass = "IZ65Hb-TBnied";
 
 // 每秒鐘檢查一次
 var checkTimer = setInterval(checkFunction, 1000);
 console.log("Start checking...");
 
 function checkFunction() {
-	// 檢查是否目標 container
-	var container = document.getElementsByClassName(targetContainerName)[0];
-	if (container != null) {
-		adjustColors(container);
+	// 檢查目標是否有出現
+	var backgrounds = document.getElementsByClassName(backgroundColorClass);
+	if (backgrounds != null) {
+		adjustColors(backgrounds);
 		clearInterval(checkTimer);
 	}
 }
 
-function adjustColors(container) {
-	var tmp = container.innerHTML;
+function adjustColors(backgrounds) {
 
-	// 替換目標顏色 (g 代表 global，這樣才會替換所有 match 的字串)
-	for (var i = 0; i < oldColors.length; i++)
-		tmp = tmp.replace(new RegExp(oldColors[i], 'g'), newColors[i]);
+	// 逐一替換每個 note 的背景
+	for (var i = 0; i < backgrounds.length; i++) {
+		var originalColor = backgrounds[i].style.backgroundColor.match(rgbColorPattern);
+		var newColor = replaceMap[originalColor];
+		backgrounds[i].style.backgroundColor = backgrounds[i].style.backgroundColor.replace(originalColor, newColor);
+	}
 
-	container.innerHTML = tmp;
 	console.log("Color are adjusted.");
 }
