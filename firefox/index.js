6dbf3f5b43fe831a4053b58261c9f2dc52feac42
slmt <sam123456777@gmail.com>
2015-09-08 19:22:52 +0800

    Add a preference option

--- firefox/index.js
+++ firefox/index.js
@@ -4,6 +4,7 @@ var googleKeepURL = "keep.google.com";
 // 載入 library
 var self = require("sdk/self");
 var tabs = require("sdk/tabs");
+var prefs = require("sdk/simple-prefs");
 
 // 監聽所有 tabs
 tabs.on("ready", logURL);
@@ -14,3 +15,8 @@ function logURL(tab) {
         });
     }
 }
+
+// 注意 preferences 狀態
+prefs.on("enableAdjusting", function(prefName) {
+    console.log("The 'enable' status has been changed to '" + prefs.prefs["enableAdjusting"] + "'");
+});
