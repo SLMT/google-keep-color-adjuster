06006fe7feb8e51df8a069fd3fe97539c4b73b25
slmt <sam123456777@gmail.com>
2015-09-11 23:36:36 +0800

    Add a enable checkbox in the settings

--- firefox/index.js
+++ firefox/index.js
@@ -6,17 +6,37 @@ var self = require("sdk/self");
 var tabs = require("sdk/tabs");
 var prefs = require("sdk/simple-prefs");
 
+// 追蹤所有連結到使用中 Google Keep 頁面的 Workers
+var workers = new Set();
+
 // 監聽所有 tabs
-tabs.on("ready", logURL);
-function logURL(tab) {
+tabs.on("ready", function(tab) {
     if (tab.url.search(googleKeepURL) > -1) {
-        tab.attach({
+        var worker = tab.attach({
             contentScriptFile: self.data.url("color-adjuster.js")
         });
+
+        // 把載入完成的頁面加入 set
+        workers.add(worker);
+        // console.log("Add a new worker, set size: " + workers.size);
+
+        // 讓頁面 unload 時自動把自己從 set 中刪除
+        worker.on("detach", function() {
+            workers.delete(worker);
+            // console.log("Delete a worker, set size: " + workers.size);
+        });
     }
-}
+});
 
 // 注意 preferences 狀態
 prefs.on("enableAdjusting", function(prefName) {
-    console.log("The 'enable' status has been changed to '" + prefs.prefs["enableAdjusting"] + "'");
+    // console.log("The 'enable' status has been changed to '" + prefs.prefs["enableAdjusting"] + "'");
+    notifyAdjustingStatus(prefs.prefs["enableAdjusting"]);
 });
+
+// 通知 Google Keep 頁面設定狀態更新
+function notifyAdjustingStatus(newStatus) {
+    workers.forEach(function(worker) {
+        worker.port.emit("enableAdjusting", newStatus);
+    });
+}
